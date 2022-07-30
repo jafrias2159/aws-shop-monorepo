690aa31ebe5f7d05a1f629602610aa809432ae72
Jorge Frias <Jorge_Frias@epam.com>
2022-07-30 15:58:03 -0500

    Import product done

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,18 +1,32 @@
 'use strict';
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3('us-east-1');
 
-module.exports.hello = async (event) => {
+const Bucket = 's3-bucket-products';
+const params = {
+  Bucket,
+  Prefix: 'uploaded/',
+  Delimiter: '/',
+};
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+};
+
+module.exports.getFileNames = async (event) => {
+  const catalogs = await s3.listObjectsV2(params).promise();
+  const files = catalogs.Contents.filter((files) => files.Size).map((file) => {
+    return `https://${Bucket}.s3.amazonaws.com/${file.Key}`;
+  });
+  const body = JSON.stringify({
+    files,
+    input: event,
+  }, null, 3);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers,
+    body,
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
