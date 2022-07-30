4a39e824fab7d93119df5bdceed20dcebf16b350
Jorge Frias <Jorge_Frias@epam.com>
2022-07-30 17:48:28 -0500

    CSV Parser added

--- import-service/fileParsers.js
+++ import-service/fileParsers.js
@@ -1,27 +1,21 @@
 'use strict';
 const AWS = require('aws-sdk');
+const csv = require('csvtojson');
+
 const s3 = new AWS.S3();
 
-const Bucket = 's3-bucket-products';
-const params = {
-  Bucket,
-  Key: 'uploaded/',
-  ContentType: 'text/cvs',
-  Expires: 60,
-};
-
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-};
-
 module.exports.importFileParser = async (event) => {
-  console.log('New File was added!!');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      msg: 'Triggered!',
-      event,
-    }),
-  };
+  const Key = event.Records[0].s3.object.key;
+  const Bucket = event.Records[0].s3.bucket.name;
+  
+  try {
+    const stream = s3.getObject({ Bucket, Key }).createReadStream();
+    // convert csv file (stream) to JSON format data
+    const csvData = await csv().fromStream(stream);
+    console.log(csvData);
+    
+  } catch (err) {
+    console.log(err);
+    callback(Error(err));
+  }
 };
